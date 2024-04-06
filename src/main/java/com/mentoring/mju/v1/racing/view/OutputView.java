5f3084902a8a92f34ead144297b63005045c6f8b
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2024-04-06 18:50:44 +0900

    feat: 두 번쨰 안내메세지 메서드 생성
    OutputView: 두 번째 안내메세지 메서드 생성

--- src/main/java/com/mentoring/mju/v1/racing/view/OutputView.java
+++ src/main/java/com/mentoring/mju/v1/racing/view/OutputView.java
@@ -1,6 +1,6 @@
 package com.mentoring.mju.v1.racing.view;
 
-import java.util.List;
+
 
 
 
@@ -12,6 +12,9 @@ public class OutputView {
     {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
     }
-
+    public void secondGuide()//두 번째 안내 메세지
+    {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
 
 }
