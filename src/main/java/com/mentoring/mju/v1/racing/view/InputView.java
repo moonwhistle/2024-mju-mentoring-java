3e707a47cdf157e0397aad62d71d6b3fb04eb87b
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2024-04-09 01:52:55 +0900

    Feat: getCarNames 메서드 작성
    InputView : getCarNames 메서드 생성

--- src/main/java/com/mentoring/mju/v1/racing/view/InputView.java
+++ src/main/java/com/mentoring/mju/v1/racing/view/InputView.java
@@ -1,9 +1,19 @@
 package com.mentoring.mju.v1.racing.view;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
     private static final Scanner input = new Scanner(System.in);
 
+    private List<String> carNames;
+    public List<String> getCarNames()
+    {
+        String inputCarNames = input.nextLine();
+        String[] carNamesArray = inputCarNames.split(",");
+        carNames = Arrays.asList(carNamesArray);
 
+        return carNames;
+    }
 
 }
