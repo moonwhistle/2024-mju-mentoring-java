4d9059f1a9cdd04fe80b3a22ffb505aba8c9419b
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2024-04-09 01:43:23 +0900

    Refactor: getCarNames 메서드 패키지 옮겨줌
    InputView -> OutputView: getCarNames 메서드 변경

--- src/main/java/com/mentoring/mju/v1/racing/view/InputView.java
+++ src/main/java/com/mentoring/mju/v1/racing/view/InputView.java
@@ -4,9 +4,6 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner input = new Scanner(System.in);
 
-    public void getCarNames()
-    {
-        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-    }
+
 
 }
--- src/main/java/com/mentoring/mju/v1/racing/view/OutputView.java
+++ src/main/java/com/mentoring/mju/v1/racing/view/OutputView.java
@@ -1,7 +1,10 @@
 package com.mentoring.mju.v1.racing.view;
 public class OutputView {
 
-    // TODO : 화면 출력 구현
 
-    
+    // TODO : 화면 출력 구현
+    public void getCarNames()
+    {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+    }
 }
