ad9da450a2f585c58676cc628255eb9578d2a8f3
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2024-04-09 03:19:48 +0900

    Refactor: 자동차 이름들 getter 생성, 차들 움직이는 메서드 생성
    RacingController: getCarNames 메서드 생성
    Cars: moveCars() 메서드 생성

--- src/main/java/com/mentoring/mju/v1/racing/controller/RacingController.java
+++ src/main/java/com/mentoring/mju/v1/racing/controller/RacingController.java
@@ -1,25 +1,18 @@
 package com.mentoring.mju.v1.racing.controller;
 
-import com.mentoring.mju.v1.racing.domain.Car;
-import com.mentoring.mju.v1.racing.domain.Cars;
 import com.mentoring.mju.v1.racing.view.InputView;
 
-import java.util.ArrayList;
 import java.util.List;
+
 
 public class RacingController {
 
         InputView inputView = new InputView();
 
-        public List<Car> makeCars()
+        public List<String> getCarNames()
         {
-            List<Car> carList = new ArrayList<>();
-            for(String carName: inputView.getCarNames())
-            {
-                Car car = new Car(carName);
-                carList.add(car);
-            }
-            return carList;
+            return inputView.getCarNames();
         }
+
 
 }
--- src/main/java/com/mentoring/mju/v1/racing/domain/Cars.java
+++ src/main/java/com/mentoring/mju/v1/racing/domain/Cars.java
@@ -6,9 +6,18 @@ import java.util.List;
 public class Cars {
 
     private final List<Car> cars;
+
     public Cars(final List<Car> cars) {
         this.cars = cars;
     }
+    public void moveCars()
+    {
+        for(Car car: cars)
+        {
+            car.moveCar();
+        }
+    }
+
 
 
 
