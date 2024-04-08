cdbd72bb64a952b06fc5cff716a69b6e07be9bc5
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2024-04-09 02:04:01 +0900

    Feat: 찬스 받는 매서드 추가
    InputView: getChane() 메서드 추가

--- src/main/java/com/mentoring/mju/v1/racing/view/InputView.java
+++ src/main/java/com/mentoring/mju/v1/racing/view/InputView.java
@@ -5,15 +5,19 @@ import java.util.Scanner;
 
 public class InputView {
     private static final Scanner input = new Scanner(System.in);
-
     private List<String> carNames;
+    private int Chance;
     public List<String> getCarNames()
     {
         String inputCarNames = input.nextLine();
         String[] carNamesArray = inputCarNames.split(",");
         carNames = Arrays.asList(carNamesArray);
-
         return carNames;
+    }
+    public int getChance()
+    {
+        Chance = input.nextInt();
+        return Chance;
     }
 
 }
