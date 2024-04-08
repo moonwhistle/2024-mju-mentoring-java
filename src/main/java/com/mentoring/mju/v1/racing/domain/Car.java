3b1931497eead2eff94511b578a3afdd765c5df5
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2024-04-09 02:24:37 +0900

    Feat: 자동차 움직이는 메서드 추가
    Car: moveCar 메서드 추가, 랜덤값 변수 선언

--- src/main/java/com/mentoring/mju/v1/racing/domain/Car.java
+++ src/main/java/com/mentoring/mju/v1/racing/domain/Car.java
@@ -1,10 +1,19 @@
 package com.mentoring.mju.v1.racing.domain;
 
+import java.util.Random;
+
 public class Car {
 
+    private final int RANDOM_NUMBER = (int)(Math.random()*9);
     private String carName;
+    private int moveCount;
     public Car(String carName)
     {
         this.carName = carName;
+    }
+    public int moveCar(Car car,int RANDOM_NUMBER)
+    {
+        car.moveCount++;
+        return moveCount;
     }
 }
