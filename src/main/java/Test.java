01a3eac471c2c6e0ef97992b32ca561da9c86c08
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2024-04-09 01:58:22 +0900

    Feat: 자동차 이름 입력받는 테스트 코드 작성

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,10 +1,14 @@
+import com.mentoring.mju.v1.racing.view.InputView;
+import com.mentoring.mju.v1.racing.view.OutputView;
 
 public class Test {
 
     public static void main(String[] args) {
+        OutputView outputView = new OutputView();
+        InputView inputView = new InputView();
 
+        outputView.getCarNames();
+        inputView.getCarNames();
     }
-
-
 
 }
--- src/main/java/com/mentoring/mju/Application.java
+++ src/main/java/com/mentoring/mju/Application.java
@@ -11,12 +11,6 @@ public class Application {
 
     public static void main(String[] args) {
 
-        RacingController racingController = new RacingController();
-        racingController.race();
-
-
     }
-
-
 
 }
