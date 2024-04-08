843e8125b40ec8c9f7d0da0b4c2197105a70fd31
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2024-04-09 03:28:35 +0900

    Feat: makeCars() 메소드, race() 메서드 생성
    RacingController: makeCar(), race() 생성

--- src/main/java/com/mentoring/mju/v1/racing/controller/RacingController.java
+++ src/main/java/com/mentoring/mju/v1/racing/controller/RacingController.java
@@ -1,17 +1,34 @@
 package com.mentoring.mju.v1.racing.controller;
 
+import com.mentoring.mju.v1.racing.domain.Car;
+import com.mentoring.mju.v1.racing.domain.Cars;
 import com.mentoring.mju.v1.racing.view.InputView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class RacingController {
 
         InputView inputView = new InputView();
+        Cars cars = new Cars(makeCars());
 
-        public List<String> getCarNames()
+        public List<Car> makeCars()
         {
-            return inputView.getCarNames();
+            List<Car> carList = new ArrayList<>();
+            for(String carName: inputView.getCarNames())
+            {
+                Car car = new Car(carName);
+                carList.add(car);
+            }
+            return carList;
+        }
+        public void race()
+        {
+            for(int i=0;i< inputView.getChance();i++)
+            {
+                cars.moveCars();
+            }
         }
 
 
